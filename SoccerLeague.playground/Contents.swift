aeaa09d93fdf1f188d52b5b06709c66540ec0e3b
Walter Allen <wltrallen2@gmail.com>
2018-07-13 21:55:53 -0500

    Created original players data structure for all 18 players.

--- SoccerLeague.playground/Contents.swift
+++ SoccerLeague.playground/Contents.swift
@@ -15,3 +15,23 @@
  those letters to the console.
  **********************************************************************/
 
+let players = [
+    [ "Name": "Joe Smith", "Height (inches)": "42", "Soccer Experience": "YES", "Guardian Name(s)": "Jim and Jan Smith"],
+    [ "Name": "Jill Tanner", "Height (inches)": "36", "Soccer Experience": "YES", "Guardian Name(s)": "Clara Tanner"],
+    [ "Name": "Bill Bon", "Height (inches)": "43", "Soccer Experience": "YES", "Guardian Name(s)": "Sara and Jenny Bon"],
+    [ "Name": "Eva Gordon", "Height (inches)": "45", "Soccer Experience": "NO", "Guardian Name(s)": "Wendy and Mike Gordon"],
+    [ "Name": "Matt Gill", "Height (inches)": "40", "Soccer Experience": "NO", "Guardian Name(s)": "Charles and Sylvia Gill"],
+    [ "Name": "Kimmy Stein", "Height (inches)": "41", "Soccer Experience": "NO", "Guardian Name(s)": "Bill and Hillary Stein"],
+    [ "Name": "Sammy Adams", "Height (inches)": "45", "Soccer Experience": "NO", "Guardian Name(s)": "Jeff Adams"],
+    [ "Name": "Karl Saygan", "Height (inches)": "42", "Soccer Experience": "YES", "Guardian Name(s)": "Heather Bledsoe"],
+    [ "Name": "Suzane Greenberg", "Height (inches)": "44", "Soccer Experience": "YES", "Guardian Name(s)": "Henrietta Dumas"],
+    [ "Name": "Sal Dali", "Height (inches)": "41", "Soccer Experience": "NO", "Guardian Name(s)": "Gala Dali"],
+    [ "Name": "Joe Kavalier", "Height (inches)": "39", "Soccer Experience": "NO", "Guardian Name(s)": "Sam and Elaine Kavalier"],
+    [ "Name": "Ben Finkelstein", "Height (inches)": "44", "Soccer Experience": "NO", "Guardian Name(s)": "Aaron and Jill Finkelstein"],
+    [ "Name": "Diego Soto", "Height (inches)": "41", "Soccer Experience": "YES", "Guardian Name(s)": "Robin and Sarika Soto"],
+    [ "Name": "Chloe Alaska", "Height (inches)": "47", "Soccer Experience": "NO", "Guardian Name(s)": "David and Jamie Alaska"],
+    [ "Name": "Arnold Willis", "Height (inches)": "43", "Soccer Experience": "NO", "Guardian Name(s)": "Claire Willis"],
+    [ "Name": "Phillip Helm", "Height (inches)": "44", "Soccer Experience": "YES", "Guardian Name(s)": "Thomas Helm and Eva Jones"],
+    [ "Name": "Les Clay", "Height (inches)": "42", "Soccer Experience": "YES", "Guardian Name(s)": "Wynonna Brown"],
+    [ "Name": "Herschel Krustofski", "Height (inches)": "45", "Soccer Experience": "YES", "Guardian Name(s)": "Hyman and Rachel Krustofski"]
+]
